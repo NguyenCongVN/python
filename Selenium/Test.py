626dfb5fcfb2cb073143a685e42a3bfea78dd82e
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2021-12-12 07:13:50 +0700

    Update skip image

--- Selenium/Test.py
+++ Selenium/Test.py
@@ -17,7 +17,20 @@ def XoaFilePath(path):
 #     except:
 #         pass
 
-with open('TelePath.txt' , 'w') as file:
-    for folder in getAllSubDir(fr'{os.getcwd()}\Tele\Tele'):
-        file.write(f'{folder}\n')
-    file.close()
+# with open('TelePath.txt' , 'w') as file:
+#     for folder in getAllSubDir(fr'{os.getcwd()}\Tele\Tele'):
+#         file.write(f'{folder}\n')
+#     file.close()
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix = 'bday ', intents = intents)
+
+@client.event
+async def on_ready():
+    guild = client.get_guild(873805775460511765)
+    memberList = guild.members
+    print(memberList)
+client.run('OTE5MjI1MDI4MzYzNTU4OTQz.YbStBQ.CW9nFQitspenp0gYyj0UEk0RNE8')
